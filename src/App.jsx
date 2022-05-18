712744bc546c8e85f641a55b2d3d7e6c822088ce
Vinicius Ribeiro <vayne99@gmail.com>
2022-05-18 09:00:07 -0300

    Start project with Vite

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Intro from './Intro'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Intro />
+    </div>
+  )
+}
+
+export default App
